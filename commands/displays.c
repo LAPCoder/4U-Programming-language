ad18147b4a4600f1e6b31f68bc0b0e061dd7e8ed
LAPCoder <97749743+LAPCoder@users.noreply.github.com>
2022-03-23 10:35:26 +0100

    Update displays.c

--- commands/displays.c
+++ commands/displays.c
@@ -7,39 +7,44 @@
 
 void clearTerminalByOSType()
 {
-    // check the OS
-    // Windows (both 32 and 64 bits)
-    #ifdef _WIN32
-    system("cls");
-    #endif
+	// check the OS
+	// Windows (both 32 and 64 bits)
+	#ifdef _WIN32
+		system("cls");
+	#endif
 
-    // Unix (Linux etc...)
-    // dosen't work on MacOS
-    #ifdef __unix__
-    system("clear");
-    #endif
+	// Unix (Linux etc...)
+	// dosen't work on MacOS
+	#ifdef __unix__
+		system("clear");
+	#endif
 
-    // MacOS
-    #ifdef __APPLE__
-    system("clear");
-    #endif
+	// MacOS
+	#ifdef __APPLE__
+		system("clear");
+	#endif
+
+	//Unknow OS
+	#if !defined _WIN32 && !defined __unix__ && !defined __APPLE__
+		printf("\033[2J\033[3J");
+	#endif
 }
 
 void displayHelp()
 {
-    clearTerminalByOSType();
+	clearTerminalByOSType();
 
-    printf("\n\t\033[0;34m 4U : All the commands\033[0;37m\n\n--help / -H   Displays the commands\n");
-    printf("--version / -V  Displays the version of the 4U compiler.");
-    system("pause>nul");
+	printf("\n\t\033[0;34m 4U : All the commands\033[0;37m\n\n--help / -H   Displays the commands\n");
+	printf("--version / -V  Displays the version of the 4U compiler.");
+	system("pause>nul");
 }
 
 void displayVersion()
 {
-   printf("Version of 4U : %s\n", VERSION);
+	printf("Version of 4U : %s\n", VERSION);
 }
 
 void displayMsgNoArgs()
 {
-    printf("\n\t\033[0;35m No arguments found, compilation terminated\n\n\033[0;37m");
+	printf("\n\t\033[0;35m No arguments found, compilation terminated\n\n\033[0;37m");
 }
